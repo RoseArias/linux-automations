dedfa8e5a625d3426668b22218f81023763128d5
Rose <40532241+RoseArias@users.noreply.github.com>
2025-02-11 15:38:50 -0800

    Create ansiblelab.sh

---
+++ ansiblelab.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Example install for later
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "set et ts=2 ai sw=2 nu
+autocmd FileType yaml setlocal cursorcolumn" >> ~/.vimrc && source ~/.vimrc
+
+echo "alias anr='ansible-navigator run --pae false -m stdout'
+alias anrs='ansible-navigator run --syntax-check --pae false -m stdout'" >> ~/.bash_profile && source ~/.bash_profile
+
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
+gnome-terminal --window --maximize
+
+exit
